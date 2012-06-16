d6143b3c6fb4150818227d13a6d95057bf57ce90
Carol Nichols <carol.nichols@gmail.com>
2012-06-16 15:46:56 -0400

    Add user method similar to world to CLI

--- lib/rst/cli.rb
+++ lib/rst/cli.rb
@@ -8,6 +8,8 @@ module Rst
         puts updates.join("\n\n")
       elsif args[0] == "user"
         if args[1]
+          updates = user(args[1], options)
+          puts updates.join("\n\n")
         else
           puts "Username is required."
           exit 1
@@ -16,13 +18,21 @@ module Rst
     end
 
     def world(params = {})
+      statuses(:messages_all, [], params)
+    end
+
+    def user(username, params = {})
+      statuses(:messages_user, username, params)
+    end
+
+    def statuses(which, args = [], params = {})
       statuses = []
       page = 0
       num = params[:num] || 20
 
       while statuses.size < num do
         page += 1
-        messages = Rst::Client.messages_all(:page => page)
+        messages = Rst::Client.send(which, args, :page => page)
 
         break if messages.empty?
 
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,32 +3,58 @@ require 'mocha'
 
 require 'rst'
 
-describe "world" do
+describe "Rst::CLI" do
   before do
     @cli = Rst::CLI
   end
 
-  it "gets the world updates" do
-    page_of_updates = 20.times.map { "Hi"}
-    Rst::Client.expects(:messages_all).once.returns(page_of_updates)
+  describe "world" do
+    it "gets the world updates" do
+      page_of_updates = 20.times.map { "Hi" }
+      Rst::Client.expects(:messages_all).once.returns(page_of_updates)
 
-    @cli.world.size.must_equal(20)
+      @cli.world.size.must_equal(20)
+    end
+
+    it "can specify a number of world updates to get" do
+      Rst::Client.stubs(:messages_all).returns(["Hi", "Bye", "Yo"])
+      @cli.world(:num => 2).size.must_equal(2)
+    end
+
+    it "gets as many updates as it can but stops if there arent more" do
+      Rst::Client.stubs(:messages_all).returns(["Hi", "Bye", "Yo"], [])
+      @cli.world(:num => 5).size.must_equal(3)
+    end
+
+    it "should have Rst::Statuses" do
+      Rst::Client.stubs(:messages_all).returns([
+        Rst::Status.new(:username => "ueoa", :text => "foo!")
+      ])
+      @cli.world(:num => 1).first.to_s.must_equal("ueoa: foo!")
+    end
   end
 
-  it "can specify a number of world updates to get" do
-    Rst::Client.stubs(:messages_all).returns(["Hi", "Bye", "Yo"])
-    @cli.world(:num => 2).size.must_equal(2)
+  describe "user" do
+    def username
+      "carols10cents"
+    end
+
+    it "gets one user's updates" do
+      page_of_updates = 20.times.map { "Hi" }
+
+      Rst::Client.expects(:messages_user).
+                  once.
+                  with(username, {:page => 1}).
+                  returns(page_of_updates)
+
+      @cli.user(username).size.must_equal(20)
+    end
+
+    it "can specify a number of one user's updates to get" do
+      Rst::Client.stubs(:messages_user).returns(["Hi", "Bye", "Yo"])
+      @cli.user(username, :num => 2).size.must_equal(2)
+    end
+
   end
 
-  it "gets as many updates as it can but stops if there arent more" do
-    Rst::Client.stubs(:messages_all).returns(["Hi", "Bye", "Yo"], [])
-    @cli.world(:num => 5).size.must_equal(3)
-  end
-
-  it "should have Rst::Statuses" do
-    Rst::Client.stubs(:messages_all).returns([
-      Rst::Status.new(:username => "ueoa", :text => "foo!")
-    ])
-    @cli.world(:num => 1).first.to_s.must_equal("ueoa: foo!")
-  end
 end
