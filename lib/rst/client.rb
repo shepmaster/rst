e223be2d7be99a6579fad6f6632c39c1d0adbc90
Carol Nichols <carol.nichols@gmail.com>
2012-06-16 21:31:24 -0400

    Change the method to resolve relative uris to take a base as an argument
    Rstat.us (and likely other sites) have uris that should be resolved against the current uri, not the root uri.

--- lib/rst/client.rb
+++ lib/rst/client.rb
@@ -6,9 +6,13 @@ module Rst
     extend self
 
     def messages_all(params = {:page => 1})
-      uri = find_a_in(root_response, :rel => "messages-all")
+      messages_all_path = find_a_in(root_response, :rel => "messages-all")
+      messages_all_uri = resolve_relative_uri(
+        :relative => messages_all_path,
+        :base     => base_uri
+      )
 
-      all_response = get_body(uri)
+      all_response = get_body(messages_all_uri)
 
       messages = all_response.css("div#messages ul.all li").map { |li|
                    Rst::Status.parse(li)
@@ -17,12 +21,19 @@ module Rst
     end
 
     def messages_user(params = {})
-      users_search_uri = find_a_in(root_response, :rel => "users-search")
+      users_search_path = find_a_in(root_response, :rel => "users-search")
+      users_search_uri = resolve_relative_uri(
+        :relative => users_search_path,
+        :base     => base_uri
+      )
 
       users_search_response = get_body(users_search_uri)
 
       form = users_search_response.css("form.users-search").first
-      search_uri = resolve_relative_uri(form["action"])
+      search_uri = resolve_relative_uri(
+        :relative => form["action"],
+        :base     => users_search_uri
+      )
 
       user_lookup_query = "#{search_uri}?search=#{params[:username]}"
 
@@ -34,7 +45,11 @@ module Rst
         sr.css("span.user-text").first.text.strip.match(/^#{params[:username]}$/i)
       }
 
-      user_uri = find_a_in(result, :rel => "user")
+      user_path = find_a_in(result, :rel => "user")
+      user_uri = resolve_relative_uri(
+        :relative => user_path,
+        :base     => user_lookup_query
+      )
 
       user_response = get_body(user_uri)
 
@@ -51,17 +66,18 @@ module Rst
 
       link = html.xpath(
         ".//a[contains(@rel, '#{params[:rel]}')]"
-      ).first
-
-      resolve_relative_uri(link["href"])
+      ).first["href"]
     end
 
     def root_response
       get_body(base_uri)
     end
 
-    def resolve_relative_uri(relative)
-      (URI(base_uri) + URI(relative)).to_s
+    def resolve_relative_uri(params = {})
+      raise "no relative uri specified" unless params[:relative]
+      raise "no base uri specified" unless params[:base]
+
+      (URI(params[:base]) + URI(params[:relative])).to_s
     end
 
     def base_uri
